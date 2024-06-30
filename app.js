cdda77a2bc95a3b2372a5d0b737d0b4adbef42c5
sumit das <sumit260799@gmail.com>
2024-06-30 13:20:03 +0530

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 // db connection......
 const connectDB = require("./config/url");
 connectDB();
